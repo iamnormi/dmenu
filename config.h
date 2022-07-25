e81725ef254fbd84aaea4da02a70f0a6eb0d7e61
iamvk1437k <automatecode@gmail.com>
2022-07-25 14:15:07 +0530

    initial Commit

---
+++ config.h
@@ -0,0 +1,41 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrains Mono:size=12",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
+};
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+/* default colors*/
+/*
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+*/
+
+/*custom draula theme*/
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#6272a4" },
+	[SchemeOut] = { "#282a36", "#8be9fd" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
